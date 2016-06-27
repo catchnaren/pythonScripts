3ebaa8e8979f712a7340621441cf0e195eab1251
catchnaren <narenckumar@gmail.com>
2016-06-27 11:25:02 +0530

    Add files via upload

---
+++ readFileChunk.py
@@ -0,0 +1,29 @@
+import os
+
+#get the input file
+def fileLoader():
+    fname = raw_input('Enter the file name: ')
+    try:
+        global fhand
+        fhand = open(fname)
+        global sz
+        sz = os.path.getsize(fname)
+    except:
+        print 'File cannot be opened: ', fname
+        exit()
+        
+#chunk process
+def fileReadChunk():
+    if sz > 0:
+        inp = sz/10000
+        fhand.seek(100, 5000)
+        print str(sz)
+        print str(inp)
+        print(fhand.read(int(inp)))
+
+def main():
+    fileLoader()
+    fileReadChunk()
+
+if __name__ == '__main__':
+    main()
